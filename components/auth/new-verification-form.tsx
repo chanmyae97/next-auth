4a51c65dc5f0f4c865a3a436b39ba61696c399e2
Ethan Chan <acm.fullstack@gmail.com>
2025-06-20 00:50:39 +0700

    email verification features

---
+++ components/auth/new-verification-form.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { BeatLoader } from "react-spinners";
+import { useSearchParams } from "next/navigation";
+import { newVerification } from "@/actions/new-verification";
+
+import { CardWrapper } from "./card-wrapper";
+import { FormError } from "../form-error";
+import { FormSuccess } from "../form-success";
+import { useCallback, useEffect, useState } from "react";
+
+export const NewVerificationForm = () => {
+  const [error, setError] = useState<string | undefined>();
+  const [success, setSuccess] = useState<string | undefined>();
+  const searchParams = useSearchParams();
+
+  const token = searchParams.get("token");
+
+  const onSubmit = useCallback(() => {
+    if (!token) {
+      setError("missing token");
+      return;
+    }
+    newVerification(token)
+      .then((data) => {
+        setSuccess(data.success);
+        setError(data.error);
+      })
+      .catch(() => {
+        setError("Something went wrong");
+      });
+  }, [token]);
+
+  useEffect(() => {
+    onSubmit();
+  }, [onSubmit]);
+  return (
+    <CardWrapper
+      headerLabel="Confirming your verification"
+      backButtonHref="/auth/login"
+      backButtonLabel="Back to login"
+      showSocial={false}
+    >
+      <div className="flex items-center w-full justify-center">
+        {!success && !error && <BeatLoader />}
+        <FormSuccess message={success} />
+        <FormError message={error} />
+      </div>
+    </CardWrapper>
+  );
+};
